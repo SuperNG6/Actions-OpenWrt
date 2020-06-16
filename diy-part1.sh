6e4170cab0c478a8d61c6261f4157fde852949da
SuperNG6 <sleele@gmail.com>
2020-06-16 14:39:30 +0800

    test

--- diy-part1.sh
+++ diy-part1.sh
@@ -8,5 +8,8 @@
 # Blog: https://p3terx.com
 #=============================================================
 
-#1. Modify default IP
-sed -i 's/192.168.1.1/192.168.1.10/g' openwrt/package/base-files/files/bin/config_generate
+# Uncomment a feed source
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# Add a feed source
+#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
