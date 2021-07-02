a3ef62cd58fabea3d494327e84b3bc7e6349929e
SuperNG6 <sleele@gmail.com>
2021-07-02 16:29:21 +0800

    update sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,3 +11,7 @@
 # sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
 sed -i '$a src-git openclash https://github.com/vernesong/OpenClash' feeds.conf.default
+
+# 修改内核为4.19
+sed -i 's/KERNEL_PATCHVER:=5.10/KERNEL_PATCHVER:=4.19/g' ./target/linux/x86/Makefile
+sed -i 's/KERNEL_TESTING_PATCHVER:=5.10/KERNEL_TESTING_PATCHVER:=4.19/g' ./target/linux/x86/Makefile
